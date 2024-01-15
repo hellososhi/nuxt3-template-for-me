a778d011fd78feb96f95721b0b3d9f2294f149d9
Soshi <22724971+hellososhi@users.noreply.github.com>
2024-01-15 18:12:39 +0900

    [add] sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
+import path from "path";
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: "src/",
+  css: ["@assets/styles/common.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@assets/styles/main.scss" as *;`,
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+      },
+    },
+  },
+  alias: {
+    "@/*": "src/*",
+    "@assets/*": "src/assets/*",
+  },
 });
